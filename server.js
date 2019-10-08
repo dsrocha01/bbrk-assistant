06f6abd363a58a46011e5889f7129948aa6176aa
Douglas da Silva Rocha <douglas.rocha@brb.com.br>
2019-10-08 16:45:14 -0300

    a

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.static("public"));
 
 app.post("/bbrk", function(request, response) {
  
-    console.log('AI!');
+    
     
   var intentName = request.body.queryResult.intent.displayName;
 
